d463ac4f54f8ee33f6ea62939bd846e0d6e2a713
EJair <ejair2709@gmail.com>
2018-04-20 22:17:38 -0500

    matriz de pesos pseudoaleatoria y se obtiene matriz de pesos final

--- perceptron_simple_images.py
+++ perceptron_simple_images.py
@@ -53,3 +53,44 @@ print(np.shape(entradas_vias))
 print("VALOR ESPERADO")
 print(valor_esperado)
 #ARREGLO DE LA MATRIZ "ALEATORIA"
+alpha=1
+value=0
+vector_pesos=[]
+##matriz de pesos aleatoria
+for x in range(len(entradas_vias[0])):
+    value=round(random.uniform(-200,200))
+    vector_pesos.append(value)
+matriz_pesos=np.array(vector_pesos).reshape(len(entradas_vias[0]),1)
+salida=0
+salida_final=0
+correcto=False
+i=0
+errores=[]
+print("MATRIZ DE PESOS INICIAL")
+print(matriz_pesos)
+while(correcto!=True):
+    for j in range(len(entradas_vias[i])):
+            salida=entradas_vias[i][j]*vector_pesos[j]
+            salida_final=salida_final+salida
+
+    if(salida_final>=0):
+        salida_final=1
+    else:
+        salida_final=-1
+    error=valor_esperado[i]-salida_final
+    if (error==0):
+        errores.append(error)
+        if(i==len(entradas_vias)-1):
+            print("MATRIZ PESOS FINAL")
+            print(matriz_pesos)
+            print("Errores")
+            print(errores)
+            correcto=True
+        i = i + 1
+
+    else:
+        for k in range(len(entradas_vias[i])):
+            vector_pesos[k]=vector_pesos[k]+(alpha*error)*entradas_vias[i][k]
+        errores.append(error)
+        matriz_pesos = np.array(vector_pesos).reshape(len(entradas_vias[i]),1)
+        i=0
